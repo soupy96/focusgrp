0e11dbe0fe255fac7b2ed988e5a74b67c1c33bc1
Michael Campbell <michael.campbell14@gmail.com>
2020-02-10 10:31:49 -0500

    making the second card in the insights work

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,20 @@
 $(window).load(function () {
-	$(".trigger_popup_fricc").click(function(){
-	   $('.hover_bkgr_fricc').show();
+	$(".trigger_popup_fricc1").click(function(){
+	   $('.hover_bkgr_fricc1').show();
 	});
-	$('.hover_bkgr_fricc').click(function(){
-		$('.hover_bkgr_fricc').hide();
+	$('.hover_bkgr_fricc1').click(function(){
+		$('.hover_bkgr_fricc1').hide();
 	});
-	$('.popupCloseButton').click(function(){
-		$('.hover_bkgr_fricc').hide();
+	$('.popupCloseButton1').click(function(){
+		$('.hover_bkgr_fricc1').hide();
 	});
+	$(".trigger_popup_fricc2").click(function(){
+		$('.hover_bkgr_fricc2').show();
+	 });
+	 $('.hover_bkgr_fricc2').click(function(){
+		 $('.hover_bkgr_fricc2').hide();
+	 });
+	 $('.popupCloseButton2').click(function(){
+		 $('.hover_bkgr_fricc2').hide();
+	 });
 });
