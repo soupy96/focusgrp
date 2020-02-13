6018e7cc42177d5898966c73ba9f1da8da73929a
Michael Campbell <michael.campbell14@gmail.com>
2020-02-13 16:33:50 -0500

    making the newsletter signup work through email

---
+++ js/nlform-scripts.js
@@ -0,0 +1,53 @@
+$("#newsletter").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        formError();
+        submitMSG(false, "You might have forgotten something. Please try again.");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm();
+    }
+});
+
+
+function submitForm(){
+    // Initiate Variables With Form Content
+    var fullname = $("#fullname").val();
+    var nlemail = $("#nlemail").val();
+
+    $.ajax({
+        type: "POST",
+        url: "php/nlform-process.php",
+        data: "fullname=" + fullname + "&nlemail=" + nlemail,
+        success : function(text){
+            if (text == "success"){
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false,text);
+            }
+        }
+    });
+}
+
+function formSuccess(){
+    // $("#contactForm")[0].reset();
+    submitMSG(true, "Thanks for subscribing!")
+}
+
+function formError(){
+    $("#newsletter").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $(this).removeClass();
+    });
+}
+
+function submitMSG(valid, msg){
+    if(valid){
+        var msgClasses = "h3 text-center tada animated text-success";
+        $("#newsletter-submit").attr("style", "display: none;");
+    } else {
+        var msgClasses = "h3 text-center text-danger";
+    }
+    $("#newsletterSubmit").removeClass().addClass(msgClasses).text(msg);
+}
---
+++ php/nlform-process.php
@@ -0,0 +1,65 @@
+<?php
+
+$errorMSG = "";
+
+// If the first name is empty or invalid
+if (empty($_POST["fullname"])) {
+    $errorMSG .= "Full Name is required";
+} else {
+    $testingfullname = test_input($_POST["fullname"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$testingfullname)) {
+        $errorMSG = "Full Name must only include letters and spaces";
+        $fullname = "";
+    } else {
+        $fullname = $_POST["fullname"];
+    }
+}
+
+// If the email is empty or invalid
+if (empty($_POST["nlemail"])) {
+    $errorMSG = "Email is required";
+} else {
+    $testingnlemail = test_input($_POST["nlemail"]);
+    if (!filter_var($testingnlemail, FILTER_VALIDATE_EMAIL)) {
+        $errorMSG = "Invalid email format";
+        $nlemail = "";
+    } else {
+        $nlemail = $_POST["nlemail"];
+    }
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$EmailTo = "michael@blackridgestrategy.com"; //This is for testing
+//$EmailTo = "laura@focusgrp.ca"; // This is for live
+$Subject = "New Newsletter Subscription!";
+
+// prepare email body text
+$Body = "";
+$Body .= "Full Name: ";
+$Body .= $fullname;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $nlemail;
+$Body .= "\n";
+
+// redirect to success page
+if ($errorMSG == ""){
+   // send email if there are no errors
+    $success = mail($EmailTo, $Subject, $Body, "From:".$nlemail);
+    // echo success for the ajax part
+    echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
